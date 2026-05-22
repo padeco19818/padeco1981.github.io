be08f35ae5c7a5dc29499fa94f8c859382a252aa
NSHIMYUMUKIZA Patrice <smithbush42@gmail.com>
2026-05-22 12:19:35 +0200

    Add async functions to save and load stories

--- api/save-story.php
+++ api/save-story.php
@@ -1,1 +1,17 @@
 
+async function saveStoryToServer(storyData) {
+    const response = await fetch('/api/save-story.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(storyData)
+    });
+    
+    return await response.json();
+}
+
+async function loadStoriesFromServer() {
+    const response = await fetch('/api/save-story.php');
+    return await response.json();
+}
